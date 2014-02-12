9de68351adb63c700deeb216bb938ec6fa7969e1
IvayloGeorgiev <georgievir@yahoo.com>
2014-02-12 18:19:55 +0000

    Added an extra line.

--- Blahblah/TestingShitIsFun/Hallo.cs
+++ Blahblah/TestingShitIsFun/Hallo.cs
@@ -11,6 +11,7 @@ namespace TestingShitIsFun
         static void Main(string[] args)
         {
             Console.WriteLine("Hello git!");
+            Console.WriteLine("Good day git!");
         }
     }
 }
